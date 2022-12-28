1025acd3963403cdd19c49b63915221b96cc3d49
antromeo <45900043+antromeo@users.noreply.github.com>
2022-12-28 17:07:34 +0100

    test9

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-console.log("Hello")
+console.log("Hello from feature flag example")
 console.log(`env are ${JSON.stringify(process.env)}`)
+
+if(process.env.feature1 === "true"){
+    console.log(`feature 1 enabled`)
+    console.log(new Date())
+}
+if(process.env.feature2 === "true"){
+    console.log(`feature 2 enabled`)
+
+    console.log(new Date().toUTCString())
+}
